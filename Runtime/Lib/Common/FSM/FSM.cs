5495b7610581ecc5c2a0b3550c166bcf089fe68b
caleb.cao <caleb.cao@ingcreations.com>
2024-04-08 12:53:18 +0800

    [add] 添加简单有限状态机

---
+++ Runtime/Lib/Common/FSM/FSM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IG.Runtime.Common.FSM{
+    /// <summary>
+    /// 状态机
+    /// </summary>
+    /// <typeparam name="TStatus">[State的状态类型]</typeparam>
+    /// <typeparam name="TState">[State的类型]</typeparam>
+    public abstract class FSM<TStatus, TState> : IDisposable{
+        public            FSMState<TStatus, TState>            CurrentState{ get; private set; }
+        protected         List<FSMTransition<TStatus, TState>> _Transitions = new List<FSMTransition<TStatus, TState>>();
+        protected         List<FSMState<TStatus, TState>>      _States      = new List<FSMState<TStatus, TState>>();
+        protected         FSMTransition<TStatus, TState>       GetTransition(TState stateId){ return _Transitions.Find(x => x.EqualCurrentState(stateId)); }
+        protected         FSMState<TStatus, TState>            GetState(TState      state)  { return _States.Find(x => x.State.Equals(state)); }
+        protected virtual void                                 OnDispose()                  { }
+
+        protected virtual void NextState(){
+            if (CurrentState != null){
+                CurrentState.Leave();
+                // Debug.Log(CurrentState.State.ToString() + " leave"); //Log不在base输出,需要看State的FSM自己输出
+                var nextState = GetNextState(CurrentState);
+                CurrentState = nextState;
+                if (nextState != null){
+                    nextState.Enter();
+                    if (nextState.IsComplete){
+                        NextState();
+                    }
+                }
+            }
+        }
+
+        private FSMState<TStatus, TState> GetNextState(FSMState<TStatus, TState> cur){
+            var status     = cur.Status;
+            var transition = GetTransition(cur.State);
+            if (transition == null){
+                // Debug.Log("Transition not found, already last state!");//TODO:Log走依赖倒置，用ILog接口,不在这里输出
+                Dispose();
+                return null;
+            }
+
+            var nextStateId = transition.Translate(status);
+            Debug.Assert(!nextStateId.Equals(default(TState)), $"{cur.GetType().Name} status [{status}]转换规则不存在");
+            var nextState = GetState(nextStateId);
+            Debug.Assert(nextState != null, $"状态[{nextStateId}]实例不存在");
+            return nextState;
+        }
+
+        public void AddTransition(FSMTransition<TStatus, TState> transition){ _Transitions.Add(transition); }
+        public void AddState(FSMState<TStatus, TState>           state)     { _States.Add(state); }
+
+        public void Dispose(){
+            _States.ForEach(x => x.Dispose());
+            _States.Clear();
+            CurrentState = null;
+            OnDispose();
+        }
+
+        public void Tick(float deltaTime){
+            if (CurrentState != null){
+                if (CurrentState.IsComplete){
+                    NextState();
+                }
+                else{
+                    CurrentState.Tick(deltaTime);
+                }
+            }
+        }
+
+        public void Setup(TState state){
+            if (CurrentState != null){
+                CurrentState.Leave();
+            }
+
+            CurrentState = GetState(state);
+            CurrentState.Enter();
+        }
+    }
+}
---
+++ Runtime/Lib/Common/FSM/FSMTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace IG.Runtime.Common.FSM{
+    /// <summary>
+    /// 设置状态转换关系
+    /// </summary>
+    /// <typeparam name="TStatus">[State的状态类型]</typeparam>
+    /// <typeparam name="TState">[State的类型]</typeparam>
+    public abstract class FSMTransition<TStatus, TState>{
+        private Dictionary<TStatus, TState> _mapping = new Dictionary<TStatus, TState>();
+        private TState                      _currentStateId;
+        private TState                      _defaultNextStateId;
+        public FSMTransition(TState          currentStateId){ this._currentStateId = currentStateId; }
+        public bool EqualCurrentState(TState inputState){ return _currentStateId.Equals(inputState); }
+
+        public FSMTransition<TStatus, TState> AddTransform(TStatus status, TState stateId){
+            _mapping.Add(status, stateId);
+            return this;
+        }
+
+        public FSMTransition<TStatus, TState> SetDefaultTransform(TState stateId){
+            _defaultNextStateId = stateId;
+            return this;
+        }
+
+        public virtual TState Translate(TStatus status){
+            if (_mapping.TryGetValue(status,out var result)){
+                return result;
+            }
+
+            return _defaultNextStateId;
+        }
+    }
+}
