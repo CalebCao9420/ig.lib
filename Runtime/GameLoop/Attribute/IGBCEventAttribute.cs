6456d88116399c6450e4f05bdc014fcb137fe190
calebcao9420 <calebcao9420@gmail.com>
2024-05-01 21:19:08 +0800

    [fix] 修改IGBC的Attribute可继承

--- Runtime/GameLoop/Attribute/IGBCEventAttribute.cs
+++ Runtime/GameLoop/Attribute/IGBCEventAttribute.cs
@@ -2,7 +2,7 @@
     using System;
 
     //TODO:后边可以考虑 接入github的 DirectAttribute，搜索速度会快上很多，看下牺牲多少内存
-    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Method)]
     public class IGBCEventAttribute : System.Attribute{
         public GameEventType EventType{ get; private set; }
         public IGBCEventAttribute(GameEventType eventType){ EventType = eventType; }
