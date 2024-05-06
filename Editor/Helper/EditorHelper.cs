c5167192c3f4ef2dfe2953b6f858f6b74884cae4
caleb.cao <caleb.cao@ingcreations.com>
2024-05-06 10:34:43 +0800

    [add] 添加简单EditorHelper,持续更新

---
+++ Editor/Helper/EditorHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace IG.Editor.Helper{
+    using UnityEditor;
+
+    public static class EditorHelper{
+        public static void VerticalPair(System.Action cb){
+            EditorGUILayout.BeginVertical();
+            cb?.Invoke();
+            EditorGUILayout.EndVertical();
+        }
+
+        public static void HorizontalPair(System.Action cb){
+            EditorGUILayout.BeginHorizontal();
+            cb?.Invoke();
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
