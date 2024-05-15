cc35ce25c54d2caea31b83a7879be9847055d712
caleb.cao <caleb.cao@ingcreations.com>
2024-05-15 16:28:13 +0800

    [changed] 更改Value操作类型

---
+++ Runtime/Base/Data/ValueOperatorType.cs
@@ -0,0 +1,7 @@
+﻿namespace IG.Data{
+    public enum ValueOperatorType{
+        Ignore     = 0, //忽略操作类型
+        Temporary  = 1, //临时的
+        Persistent = 2, //永久的
+    }
+}
--- Runtime/Base/Data/ValueType.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace IG.Data{
-    public enum ValueType{
-        Temporary   = 0, //临时的
-        Persistent = 1, //永久的
-    }
-}
