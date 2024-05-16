1d4f2b5d8aad98708be73d51a560b313932da78f
caleb.cao <caleb.cao@ingcreations.com>
2024-05-16 09:42:30 +0800

    [add] 增加Float扩展类

---
+++ Runtime/Lib/Extensions/FloatExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace IG.Runtime.Extensions{
+    public static class FloatExtension{
+        /// <summary>
+        /// 可以抽到工具方法里边去 
+        /// </summary>
+        public static float CullOverflowsVal(this float increase, float total, float target = 1.0f){
+            if (total + increase >= target){
+                increase = target - total;
+            }
+
+            return increase;
+        }
+    }
+}
