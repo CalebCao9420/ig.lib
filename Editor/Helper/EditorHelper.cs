f2d9f0ddbd943193a3963053e7f5539308f1080e
calebcao9420 <calebcao9420@gmail.com>
2024-08-01 00:16:50 +0800

    [add] Increase new dropdown normal function in ig.lib

--- Editor/Helper/EditorHelper.cs
+++ Editor/Helper/EditorHelper.cs
@@ -7,23 +7,66 @@ namespace IG.Editor.Helper{
     using UnityEditor;
 
     public static class EditorHelper{
+        /// <summary>
+        /// 纵向布局
+        /// </summary>
+        /// <param name="cb"></param>
         public static void VerticalPair(System.Action cb){
             EditorGUILayout.BeginVertical();
             cb?.Invoke();
             EditorGUILayout.EndVertical();
         }
 
+        /// <summary>
+        /// 横向布局
+        /// </summary>
+        /// <param name="cb"></param>
         public static void HorizontalPair(System.Action cb){
             EditorGUILayout.BeginHorizontal();
             cb?.Invoke();
             EditorGUILayout.EndHorizontal();
         }
 
+        /// <summary>
+        /// Scroll
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="cb"></param>
+        /// <param name="showHorizontal"></param>
+        /// <param name="showVertical"></param>
+        /// <returns></returns>
         public static Vector2 Scroll(Vector2 pos, System.Action cb, bool showHorizontal, bool showVertical){
             pos = GUILayout.BeginScrollView(pos, showHorizontal, showVertical);
             cb?.Invoke();
             GUILayout.EndScrollView();
             return pos;
+        }
+
+        /// <summary>
+        /// Dropdown 菜单
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="content"></param>
+        /// <param name="check"></param>
+        /// <param name="onSelect"></param>
+        /// <param name="style"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void DropdownMenu<T>(string label, T[] content, Func<T, bool> check, GenericMenu.MenuFunction2 onSelect, GUIStyle style){
+            if (GUILayout.Button(label, style)){
+                int         len  = content?.Length ?? 0;
+                GenericMenu menu = null;
+                if (len > 0){
+                    menu = new GenericMenu();
+                }
+
+                for (int i = 0; i < len; ++i){
+                    var        single     = content[i];
+                    GUIContent guiContent = new GUIContent($"{single}");
+                    menu.AddItem(guiContent, check.Invoke(single), onSelect, single);
+                }
+
+                menu.ShowAsContext();
+            }
         }
 
         public static string HasTitleSelectFolderPathHorizontal(string title, string content, float width = 100.0f, float height = 100f){
