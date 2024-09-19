5311cc6646914358f2e3bfe76e93449e337ec1ac
PC_Caleb <calebcao9420@gmail.com>
2024-09-19 11:07:17 +0800

    [add] Increase EnumExtension to fixed enum function

---
+++ Runtime/Lib/Extensions/EnumExtension.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IG.Runtime.Extensions{
+    public static class EnumExtension{
+        public static T GetByIndex<T>(this     T      en,   int index) where T : Enum{ return (T)Enum.ToObject(en.GetType(), index); }
+        public static T GetEnumByIndex<T>(this Type   type, int index) where T : Enum{ return (T)Enum.ToObject(type,         index); }
+        public static T ToEnum<T>(this         string name) where T : System.Enum{ return (T)Enum.Parse(typeof(T), name); }
+    }
+}
